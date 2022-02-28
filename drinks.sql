9ac9467af0516ada6b1657c44cc4825a38d9fe55
Carly Burroughs <burroughs.carlyle@gmail.com>
2022-02-28 18:41:42 -0500

    Added drinks.sql for creating and populating drinks table in postgres database

---
+++ drinks.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS drinks (
+    id VARCHAR(50) PRIMARY KEY,
+    name VARCHAR(255),
+    altName VARCHAR(255),
+    tags VARCHAR(255),
+    videoUrl VARCHAR(500),
+    category VARCHAR(50),
+    alcoholStatus VARCHAR(50),
+    glass VARCHAR(50),
+    instructions VARCHAR,
+    thumbnailUrl VARCHAR(500),
+    imgSrc VARCHAR(500),
+    imgAttribution VARCHAR(255),
+    ccConfirmed BOOLEAN,
+    dateModified TIMESTAMP,
+    recipe VARCHAR
+);
+
+\copy drinks FROM 'drinks.csv' WITH (format csv, header false, delimiter ',');
