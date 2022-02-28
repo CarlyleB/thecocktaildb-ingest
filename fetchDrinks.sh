26a1dbb593f4bac2ac0f9357a98974f6508f1a7c
Carly Burroughs <burroughs.carlyle@gmail.com>
2022-02-28 17:26:55 -0500

    Convert json to sql

--- fetchDrinks.sh
+++ fetchDrinks.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
 
-content=$(curl -s  -X GET -H "Header:Value" curl https://www.thecocktaildb.com/api/json/v2/$1/search.php?f=a)
-transformed=$( jq -r --arg ingredientPrefix 'strIngredient' --arg measurementPrefix 'strMeasure' '[.drinks[] |
-	{
-		id: .idDrink,
-		name: .strDrink,
-		altName: .strDrinkAlternate,
-		tags: .strTags,
-		videoUrl: .strVideo,
-		category: .strCategory,
-		alcoholStatus: .strAlcoholic,
-		glass: .strGlass,
-		instructions: .strInstructions,
-		thumbnailUrl: .strDrinkThumb,
-		imgSrc: .strImageSource,
-		imgAttribution: .strImageAttribution,
-		ccConfirmed: .strCreativeCommonsConfirmed,
-		dateModified: .dateModified,
-		recipe: [(. as $original
-			| ($original | keys) as $keys
-			| $keys[]
-			| select(startswith($ingredientPrefix) and $original[.] != null) as $ingredientKeys
-			| [$ingredientKeys] | map({
-				ingredient: $original[.],
-				measurement: $original[$measurementPrefix + .[-1:]]
-			})[]
-		)]
-	}]' <<< "${content}" )
+apiKey="${1:-1}"
 
-echo "${transformed}"
+if [ ! -z $1 ] 
+then 
+    apiVersion="v1"
+else
+    apiVersion="v2"
+fi
+
+baseUrl="https://www.thecocktaildb.com/api/json/${apiVersion}/${apiKey}"
+
+fetchAllDrinks () {
+	url="$baseUrl/search.php?f="
+
+	urls=""
+	for letter in {a..z}
+	do
+		urls+=" ${url}${letter}"
+	done
+
+	content=$(curl -s $urls)
+	
+	transformed=$( jq -r '.drinks[]? |
+		{
+			id: .idDrink,
+			name: .strDrink,
+			altName: .strDrinkAlternate,
+			tags: .strTags,
+			videoUrl: .strVideo,
+			category: .strCategory,
+			alcoholStatus: .strAlcoholic,
+			glass: .strGlass,
+			instructions: .strInstructions,
+			thumbnailUrl: .strDrinkThumb,
+			imgSrc: .strImageSource,
+			imgAttribution: .strImageAttribution,
+			ccConfirmed: .strCreativeCommonsConfirmed,
+			dateModified: .dateModified,
+			recipe: ([(. as $original
+				| ($original | keys) as $keys
+				| $keys[]
+				| select(startswith("strIngredient") and $original[.] != null) as $ingredientKeys
+				| [$ingredientKeys] | map({
+					ingredient: $original[.],
+					measurement: $original["strMeasure" + .[-1:]]
+				})[]
+			)]) | tostring
+		} | flatten | @csv' <<< "${content}"
+	)
+
+	echo "${transformed}"
+}
+
+fetchAllDrinks
